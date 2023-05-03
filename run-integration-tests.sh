9b29518d36b130ba8f218b30342bca599b5446df
Ben Sherred <ben@sherred.co.uk>
2023-05-03 22:26:30 +0100

    chore: remove support for Laravel 8

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -7,22 +7,9 @@ function _test-laravel-version() {
   )
 }
 
-if [[ $(php -i | grep "PHP Version => 7") ]]; then
-  echo "Testing Laravel 7.x..."
-  _test-laravel-version "7.x"
-else
-  echo "Skipping Laravel 7.x tests -- PHP version too high"
-fi
-
 if [[ $(php -i | grep "PHP Version => 8") ]]; then
-  echo "Testing Laravel 8.x..."
-  _test-laravel-version "8.x"
-
   echo "Testing Laravel 9.x..."
   _test-laravel-version "9.x"
 else
-  echo "Skipping Laravel 8.x and 9.x tests -- PHP version too low"
+  echo "Skipping Laravel 9.x tests -- PHP version too low"
 fi
-
-
-
